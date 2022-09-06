d2325e696d45e7214aa9e988f1e24c60cd659394
infinissible <infinissible@gmail.com>
2022-09-05 18:05:32 -0700

    nav bar and video added

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Navigation';
+import Main from './Main';
+
+function Header() {
+  return (
+    <header>
+      <Nav />
+      <Main />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import main from '../assets/jpg/main.jpeg';
+import video from '../assets/video/wedding.mp4';
+
+function Main() {
+  return (
+    <div>
+      <h1>이승진 ❤️ 이길주</h1>
+      <img width={500} alt='main' src={main} />
+      <h2>승진 길주 결혼식에 당신을 초대 합니다.</h2>
+      <h2>
+        2022. 10. 8. 토요일 오후 12:00 <br />
+        전주 향교
+      </h2>
+      <video width={500} controls>
+        <source src={video} type='video/mp4' />
+      </video>
+    </div>
+  );
+}
+
+export default Main;
